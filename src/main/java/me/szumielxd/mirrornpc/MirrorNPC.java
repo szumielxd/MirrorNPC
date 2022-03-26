ad37958d5669d3fd403e872ae406f22d79ef7d97
szumielxd <43210079+szumielxd@users.noreply.github.com>
2022-03-26 22:15:37 +0100

    Fixed spigot support

--- src/main/java/me/szumielxd/mirrornpc/MirrorNPC.java
+++ src/main/java/me/szumielxd/mirrornpc/MirrorNPC.java
@@ -1,8 +1,15 @@
 package me.szumielxd.mirrornpc;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -35,6 +42,15 @@ public class MirrorNPC extends JavaPlugin implements Listener {
 	private ProtocolManager protocolManager = null;
 	
 	
+	private static final String NMS = Bukkit.getServer().getClass().getName().split("\\.")[3];
+	private static final Class<?> CraftWorld = Optional.of(String.format("org.bukkit.craftbukkit.%s.CraftWorld", NMS)).map(clazz -> { try { return Class.forName(clazz); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	private static final Class<?> WorldServer = Optional.of(String.format("net.minecraft.server.%s.WorldServer", NMS)).map(clazz -> { try { return Class.forName(clazz); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	private static final Class<?> NMSEntity = Optional.of(String.format("net.minecraft.server.%s.Entity", NMS)).map(clazz -> { try { return Class.forName(clazz); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	private static final Method CraftWorld_getHandle = Optional.of("getHandle").map(method -> { try { return CraftWorld.getMethod(method); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	private static final Method WorldServer_getEntity = Optional.of("getEntity").map(method -> { try { return WorldServer.getMethod(method, UUID.class); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	private static final Method NMSEntity_getBukkitEntity = Optional.of("getBukkitEntity").map(method -> { try { return NMSEntity.getMethod(method); } catch (Exception e) { throw new RuntimeException(e); } }).get();
+	
+	
 	@Override
 	public void onEnable() {
 		instance = this;
@@ -51,28 +67,31 @@ public class MirrorNPC extends JavaPlugin implements Listener {
 					if (packet.getPlayerInfoAction().read(0).equals(PlayerInfoAction.ADD_PLAYER)) {
 						List<PlayerInfoData> dataList = packet.getPlayerInfoDataLists().read(0);
 						dataList.forEach(data -> {
-							NPC npc = CitizensAPI.getNPCRegistry().getByUniqueId(data.getProfile().getUUID());
-							if (npc != null && npc.hasTrait(MirrorSkinTrait.class)) {
-								MirrorSkinTrait trait = npc.getTraitNullable(MirrorSkinTrait.class);
-								if (trait.hasMirrorSkin()) {
-									List<WrappedSignedProperty> textures = new ArrayList<>(data.getProfile().getProperties().get("textures"));
-									textures.removeIf(val -> val.getName().equals("textures"));
-									try {
-										GameProfile profile = (GameProfile) event.getPlayer().getClass().getMethod("getProfile").invoke(event.getPlayer());
-										List<Property> properties = new ArrayList<>(profile.getProperties().get("textures"));
-										Property texture = null;
-										if (!properties.isEmpty()) for (Property prop : properties) {
-											if (prop.getName().equals("textures")) {
-												texture = prop;
-												break;
+							Entity entity = getEntity(data.getProfile().getUUID());
+							if (entity != null) {
+								NPC npc = CitizensAPI.getNPCRegistry().getNPC(entity);
+								if (npc != null && npc.hasTrait(MirrorSkinTrait.class)) {
+									MirrorSkinTrait trait = npc.getTraitNullable(MirrorSkinTrait.class);
+									if (trait.hasMirrorSkin()) {
+										List<WrappedSignedProperty> textures = new ArrayList<>(data.getProfile().getProperties().get("textures"));
+										textures.removeIf(val -> val.getName().equals("textures"));
+										try {
+											GameProfile profile = (GameProfile) event.getPlayer().getClass().getMethod("getProfile").invoke(event.getPlayer());
+											List<Property> properties = new ArrayList<>(profile.getProperties().get("textures"));
+											Property texture = null;
+											if (!properties.isEmpty()) for (Property prop : properties) {
+												if (prop.getName().equals("textures")) {
+													texture = prop;
+													break;
+												}
 											}
+											if (texture != null) textures.add(WrappedSignedProperty.fromValues("textures", texture.getValue(), texture.getSignature()));
+											data.getProfile().getProperties().replaceValues("textures", textures);
+											
+										} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+												| NoSuchMethodException | SecurityException e) {
+											e.printStackTrace();
 										}
-										if (texture != null) textures.add(WrappedSignedProperty.fromValues("textures", texture.getValue(), texture.getSignature()));
-										data.getProfile().getProperties().replaceValues("textures", textures);
-										
-									} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
-											| NoSuchMethodException | SecurityException e) {
-										e.printStackTrace();
 									}
 								}
 							}
@@ -81,6 +100,20 @@ public class MirrorNPC extends JavaPlugin implements Listener {
 				}
 			}
 		});
+	}
+	
+	
+	private static Entity getEntity(UUID uuid) {
+		for (World world : Bukkit.getWorlds()) {
+			try {
+				Object nmsWorld = CraftWorld_getHandle.invoke(world);
+				Object entity = WorldServer_getEntity.invoke(nmsWorld, uuid);
+				if (entity != null) return (Entity) NMSEntity_getBukkitEntity.invoke(entity);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return null;
 	}
 	
 	
